bb51bec5d1f89baf94e49862a07cc1ffa7105b3a
Tomasz Chrost <tomaszbrianchrost@gmail.com>
2021-08-31 21:50:17 +0100

    added base logging for flask

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,7 @@ from flask_config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import logging
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -11,5 +12,6 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 # set to what url_for() should call
 login.login_view = 'login'
+logging.basicConfig(filename='full.log', encoding='utf-8', level=logging.DEBUG)
 
 from app import routes, models
