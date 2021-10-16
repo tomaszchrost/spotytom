05299dfa48822bdc4cd50cb35ea0b437cb2afa4f
Tomasz Chrost <tomaszbrianchrost@gmail.com>
2021-10-16 21:46:11 +0100

    added windows run daily script

---
+++ scripts/run_daily.bat
@@ -0,0 +1,5 @@
+setlocal
+set PYTHONPATH=C:\Users\Tomasz\PycharmProjects\spotytom
+C:\Users\Tomasz\PycharmProjects\spotytom\venv\Scripts\python.exe C:/Users/Tomasz/PycharmProjects/spotytom/scripts/update_all_best_ofs.py
+C:\Users\Tomasz\PycharmProjects\spotytom\venv\Scripts\python.exe C:/Users/Tomasz/PycharmProjects/spotytom/scripts/update_all_discovers.py
+endlocal
